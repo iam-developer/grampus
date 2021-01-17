d78315ef2265287bdb36040a4e441e1967376162
iam-developer <turgunbaaev@gmail.com>
2021-01-17 18:49:37 +0300

    Finished all pages

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // При наведении на меню:
+  $(".menu--link").hover(
+    function () {
+      $(this).addClass("active");
+    },
+    function () {
+      $(this).removeClass("active");
+      $(".active--menu").addClass("active");
+    }
+  );
 
-// При наведении на меню:
-$('.menu--link').hover(function() {
-    $(this).addClass('active');
-},
-function() {
-    $(this).removeClass('active');
-    $('.active--menu').addClass('active');
-});
+  //   Burger menu:
+  $(".burger--menu").on("click", function () {
+    $(".fixed--menu").addClass("active");
+  });
 
-// Главная страница. Слайдер:
-$(".slider--container").slick({
+  $(".close--menu").on("click", function () {
+    $(".fixed--menu").removeClass("active");
+  });
+
+  // Страница "Продукция" при наведении на блоки:
+
+  $(".product--block").hover(
+    function () {
+      $(this).find('.product--img').addClass('active');
+      $(this).find('.product--block--link').addClass('active');
+    },
+    function () {
+      $(this).find('.product--img').removeClass("active");
+      $(this).find('.product--block--link').removeClass('active');
+    }
+  );
+
+  // Главная страница. Слайдер:
+  $(".slider--container").slick({
     infinite: true,
     dots: true,
     arrow: false,
@@ -20,25 +43,14 @@ $(".slider--container").slick({
     // autoplay: true,
     autoplaySpeed: 1800,
     prevArrow: "<img class='left__arrow' src='img/left-arrow.png'>",
-    nextArrow: "<img class='right__arrow' src='img/right-arrow.png'>"
+    nextArrow: "<img class='right__arrow' src='img/right-arrow.png'>",
   });
-
-//   Burger menu:
-
-$('.burger--menu').on('click', function() {
-    $('.fixed--menu').addClass('active');
-});
-
-$('.close--menu').on('click', function() {
-    $('.fixed--menu').removeClass('active');
-});
-
 });
 
 // Preloader
 
-let preloader = document.getElementById('preloader');
+let preloader = document.getElementById("preloader");
 
-window.onload = function() {
-  preloader.style.display = 'none';
-}
+window.onload = function () {
+  preloader.style.display = "none";
+};
